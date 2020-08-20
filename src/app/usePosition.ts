d9c6f78c2bf5ca4d1a55dfac4d246c7596d8356f
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2020-08-20 09:39:44 +0200

    fixes

--- src/app/usePosition.ts
+++ src/app/usePosition.ts
@@ -5,17 +5,6 @@ export const usePosition = () => {
 	const [position, setPosition] = useState<LatLngLiteral | null>(null);
 	const [error, setError] = useState<null | string>(null);
 
-	const onChange = ({ coords }: Position) => {
-		setPosition({
-			lat: coords.latitude,
-			lng: coords.longitude,
-		});
-	};
-
-	const onError = (error: PositionError) => {
-		setError(error.message);
-	};
-
 	useEffect(() => {
 		const geo = navigator.geolocation;
 
@@ -30,10 +19,6 @@ export const usePosition = () => {
 				lat: coords.latitude,
 			});
 		});
-
-		const watcher = geo.watchPosition(onChange, onError);
-
-		return () => geo.clearWatch(watcher);
 	}, []);
 
 	return { position, error };
